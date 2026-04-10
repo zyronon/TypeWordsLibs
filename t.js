a2bb02eef7917c6b9e90aaa4d298f862febee1d9
Zyronon <zyronon@163.com>
2026-04-11 04:02:27 +0800

    s

---
+++ t.js
@@ -0,0 +1,42 @@
+let RESOURCE_URL = 'https://libs.typewords.cc/'
+  //51.la
+;(function () {
+  window.LA = window.LA || {
+    ids: [{id: '3OH8ITYRgwzo58L2', ck: '3OH8ITYRgwzo58L2'}],
+    id: '3OH8ITYRgwzo58L2',
+    ck: '3OH8ITYRgwzo58L2',
+    hashMode: true,
+  }
+  const script = document.createElement('script')
+  script.src = RESOURCE_URL + `51.js`
+  document.head.appendChild(script)
+})()
+
+// baidu
+var _hmt = _hmt || []
+;(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?3dae52fcd5375a19905462e4ad3eb54e'
+  document.head.appendChild(hm)
+})()
+
+// umami
+// ;(function () {
+//   var umami2 = document.createElement('script')
+//   umami2.src = ENV.RESOURCE_URL + 'libs/s.js'
+//   umami2.setAttribute('data-website-id', '160308c9-7900-4b1d-a0b1-c3b25a9530f6')
+//   document.head.appendChild(umami2)
+// })()
+
+// umami-saas
+;(function () {
+  var umami2 = document.createElement('script')
+  umami2.src = RESOURCE_URL + '/s.js'
+  // umami2.setAttribute('data-website-id', '03102800-e8e8-40a2-addf-9999d5e5c525')
+  umami2.setAttribute('data-website-id', 'e725f66a-1f63-4c53-a96b-ba073edecac8')
+  umami2.setAttribute('data-host-url', 'https://um.typewords.cc/')
+  umami2.setAttribute('data-do-not-track', false)
+  umami2.setAttribute('data-auto-track', true)
+  document.head.appendChild(umami2)
+})()
+
